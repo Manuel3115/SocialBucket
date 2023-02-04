19f260f461f07ff8d4e37b5babea5f3c560b193b
BFaras <71102846+BFaras@users.noreply.github.com>
2023-02-04 14:42:51 -0500

    fdds

---
+++ client/src/app/modules/material.module.ts
@@ -0,0 +1,18 @@
+
+import { NgModule } from '@angular/core';
+
+
+const modules = [
+    
+];
+
+/**
+ * Material module
+ * IMPORTANT : IMPORT ONLY USED MODULES !!!!!!
+ */
+@NgModule({
+    imports: [...modules],
+    exports: [...modules],
+    providers: [],
+})
+export class AppMaterialModule {}
