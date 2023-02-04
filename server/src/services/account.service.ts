222656ec2fd62c63b446c29858158ce92ac5260a
rak553 <raphael62243@gmail.com>
2023-02-04 18:07:29 -0500

    add bucket item and check as done bucket item

--- server/src/services/account.service.ts
+++ server/src/services/account.service.ts
@@ -1,13 +1,39 @@
 import * as io from 'socket.io';
+import { BucketItem } from '../interface/bucket_item';
+import { DatabaseService } from './database.service';
 
 export class AccountService {
+    private databaseService : DatabaseService;
+
+    constructor(databaseService : DatabaseService)
+    {   
+        this.databaseService = databaseService;
+    }
+
     handleAccountSockets(socket: io.Socket) {
-        socket.on('Bucket Add', (bucketName: string) => {
-            // TODO
+        socket.on('Bucket Add', async (bucketName: string) => {
+            let itemAdded = false;
+            if (!(socket.data.user.bucketList.findIndex((item : BucketItem) => item.name === bucketName) >= 0))
+            {
+                const bucketItem : BucketItem = { name : bucketName, isDone : false};
+                socket.data.user.bucketList.push(bucketItem);
+                await this.databaseService.updateAccountInformations(socket.data.user);
+                itemAdded = true;
+            }
+            socket.emit('Bucket Added', itemAdded);
         });
 
-        socket.on('Bucket Check', (bucketName: string) => {
+        socket.on('Bucket Check', async (bucketName: string) => {
             // TODO
+            let itemChecked = false;
+            if (socket.data.user.bucketList.findIndex((item : BucketItem) => item.name === bucketName) >= 0)
+            {
+                const bucketItem : BucketItem = { name : bucketName, isDone : false};
+                socket.data.user.bucketList.find((item : BucketItem) => item.name = bucketName).isDone = true;
+                await this.databaseService.updateAccountInformations(socket.data.user);
+                itemChecked = true;
+            }
+            socket.emit('Bucket Checked', itemChecked);
         });
 
         socket.on('Bucket Remove', (bucketName: string) => {
