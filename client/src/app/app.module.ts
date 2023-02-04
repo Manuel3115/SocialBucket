dac583a0ff12e3ed5898b0e10fe15f6088733976
BFaras <71102846+BFaras@users.noreply.github.com>
2023-02-04 14:24:38 -0500

    routing added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { AccountPageComponent } from './pages/account-page/account-page.component';
+import { AppRoutingModule } from './modules/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AccountPageComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
