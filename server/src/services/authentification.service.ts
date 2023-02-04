a6ebd0eaab2844fde6724a3e534149abb3e23105
Manuel Ramirez <manuel-etudes@oulook.com>
2023-02-04 14:21:14 -0500

    Moved Database Service Initialisation

--- server/src/services/authentification.service.ts
+++ server/src/services/authentification.service.ts
@@ -1,13 +1,12 @@
 import { BucketItem } from "../interface/bucket_item";
 import { DatabaseService } from "./database.service";
-import { Service } from 'typedi';
 
 export class AuthentificationService {
     private databaseService : DatabaseService;
 
-    constructor()
-    {
-        this.databaseService = new DatabaseService();
+    constructor(databaseService : DatabaseService)
+    {   
+        this.databaseService = databaseService;
     }
 
     createAccount(username : string, bucketlist: BucketItem[])
