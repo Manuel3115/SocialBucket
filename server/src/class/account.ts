02a7b6f2d1d0a2aa91eb00e05f15a260dc924492
rak553 <raphael62243@gmail.com>
2023-02-04 14:11:15 -0500

    Classes serveur

--- server/src/class/account.ts
+++ server/src/class/account.ts
@@ -1,9 +1,11 @@
+import { BucketItem } from "../interface/bucket_item";
+
 export class Account
 {
     public username : string;
-    public bucketlist : string[];
+    public bucketlist : BucketItem[];
 
-    constructor(user : string, list : string[])
+    constructor(user : string, list : BucketItem[])
     {
         this.username = user;
         this.bucketlist = list;
