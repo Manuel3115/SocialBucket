e3f68e679a0ea8a198f2fdb7a8665bf839d52c65
Manuel Ramirez <manuel.ramirez-evrard@polymtl.ca>
2023-02-05 02:47:22 -0500

    Small test change

--- server/src/constants/database-environment.ts
+++ server/src/constants/database-environment.ts
@@ -1,2 +1,2 @@
-export const DATABASE_URL = 'mongodb+srv://admin:12345@cluster0.tsdktbk.mongodb.net/?retryWrites=true&w=majority';
+export const DATABASE_URL = 'mongodb+srv://admin:12345@cluster0.tsdktbk.mongodb.net/';
 export const DATABASE_NAME = 'SocialBucket';
