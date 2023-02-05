4978d17de2c8bc1a1d2b752b4ad9f7baddc681be
Manuel Ramirez <manuel-etudes@oulook.com>
2023-02-04 23:04:58 -0500

    Added logo to different pages

--- client/src/assets/objectives.ts
+++ client/src/assets/objectives.ts
@@ -1,9 +1,18 @@
 export const objectives = [
-    {name:"Get in shape",
-    description:"Go to the gym for the first time"},
-    {name:"Climb Mount Everest",
-    description:"Mount Everest"},
-    {name:"Learn to cook",
-    description:"Learn how to cook at least 5 dishes"},
-    {name:"New country",
-    description:"Fo to a country where you have never been before"}]
+    {
+        name:"Get in shape",
+        description:"Go to the gym for the first time"
+    },
+    {
+        name:"Climb Mount Everest",
+        description:"Mount Everest"
+    },
+    {
+        name:"Learn to cook",
+        description:"Learn how to cook at least 5 dishes"
+    },
+    {
+        name:"New country",
+        description:"Go to a country where you have never been before"
+    }
+]
